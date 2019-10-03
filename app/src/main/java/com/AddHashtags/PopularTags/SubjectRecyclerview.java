10ee8b584ff2b15f07a08dcabc5813a7894f040e
Kang Young Gyu <kang_yg@naver.com>
2019-10-03 23:40:06 +0900

    popular tag 아이템 추가

--- app/src/main/java/com/AddHashtags/PopularTags/SubjectRecyclerview.java
+++ app/src/main/java/com/AddHashtags/PopularTags/SubjectRecyclerview.java
@@ -1,13 +1,11 @@
 package com.AddHashtags.PopularTags;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +14,7 @@ import com.example.addhashtags.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class SubjectRecyclerview extends Fragment {
@@ -50,30 +49,40 @@ public class SubjectRecyclerview extends Fragment {
                 break;
             case 1:
                 List<String> loveList = Arrays.asList(getResources().getStringArray(R.array.love));
+                Collections.sort(loveList);
+                Collections.reverse(loveList);
                 for(int i = 0 ; i < loveList.size() ; i++){
                     subjectItems.add(new SubjectItem(loveList.get(i),false));
                 }
                 break;
             case 2:
                 List<String> dailyList = Arrays.asList(getResources().getStringArray(R.array.daily));
+                Collections.sort(dailyList);
+                Collections.reverse(dailyList);
                 for(int i = 0 ; i < dailyList.size() ; i++){
                     subjectItems.add(new SubjectItem(dailyList.get(i),false));
                 }
                 break;
             case 3:
-                List<String> selfieList = Arrays.asList(getResources().getStringArray(R.array.selfie));
-                for(int i = 0 ; i < selfieList.size() ; i++){
-                    subjectItems.add(new SubjectItem(selfieList.get(i),false));
+                List<String> photoList = Arrays.asList(getResources().getStringArray(R.array.photo));
+                Collections.sort(photoList);
+                Collections.reverse(photoList);
+                for(int i = 0 ; i < photoList.size() ; i++){
+                    subjectItems.add(new SubjectItem(photoList.get(i),false));
                 }
                 break;
             case 4:
                 List<String> foodList = Arrays.asList(getResources().getStringArray(R.array.food));
+                Collections.sort(foodList);
+                Collections.reverse(foodList);
                 for(int i = 0 ; i < foodList.size() ; i++){
                     subjectItems.add(new SubjectItem(foodList.get(i),false));
                 }
                 break;
             case 5:
                 List<String> tripList = Arrays.asList(getResources().getStringArray(R.array.trip));
+                Collections.sort(tripList);
+                Collections.reverse(tripList);
                 for(int i = 0 ; i < tripList.size() ; i++){
                     subjectItems.add(new SubjectItem(tripList.get(i),false));
                 }
