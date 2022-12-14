8bf318d3f775df8b707cb251635f201e6876b482
Kang Young Gyu <kang.yg.dev@gmail.com>
2022-12-14 13:34:50 +0900

    * MineFragment - View development

--- app/src/main/java/com/tagplus/addhashtags/View/Mine/MineFragment.kt
+++ app/src/main/java/com/tagplus/addhashtags/View/Mine/MineFragment.kt
@@ -2,10 +2,13 @@ package com.tagplus.addhashtags.View.Mine
 
 import android.os.Bundle
 import android.view.View
-import com.tagplus.addhashtags.databinding.FragmentMineBinding
+import androidx.fragment.app.viewModels
 import com.tagplus.addhashtags.View.BaseFragment
+import com.tagplus.addhashtags.ViewModel.MineFragmentViewModel
+import com.tagplus.addhashtags.databinding.FragmentMineBinding
 
 class MineFragment : BaseFragment<FragmentMineBinding>(FragmentMineBinding::inflate) {
+    private val mineFragmentViewModel: MineFragmentViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
